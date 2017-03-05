fafd03f039db0bc0520f8ae8657d68753bfa2410
Smriti Gupta <smritigupta2202@gmail.com>
2017-03-05 23:39:45 +0530

    Add files via upload

---
+++ Love.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Love {
+	public static void main(String []args)
+	{
+		int x=0;
+		while(x<4)
+		{
+			System.out.println("Love u soooo muchhhhhhh babuuuuu....");
+		}
+	}
+
+}
