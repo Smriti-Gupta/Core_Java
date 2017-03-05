4ed55e45bf526eda1851db499196a1ee577d229f
Smriti Gupta <smritigupta2202@gmail.com>
2017-03-05 23:41:59 +0530

    Update and rename Love.java to While_loop.java

--- While_loop.java
+++ While_loop.java
@@ -1,11 +1,11 @@
 import java.util.*;
-public class Love {
+public class While_loop {
 	public static void main(String []args)
 	{
 		int x=0;
 		while(x<4)
 		{
-			System.out.println("Love u soooo muchhhhhhh babuuuuu....");
+			System.out.println("While Loop ran : Iteration "+i);
 		}
 	}
 
