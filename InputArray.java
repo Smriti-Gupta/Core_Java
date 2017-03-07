f8396fa8d040aa2b02c57a3a1a2aea6cc8724bdd
Smriti Gupta <smritigupta2202@gmail.com>
2017-03-07 23:05:44 +0530

    Add files via upload

---
+++ InputArray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class InputArray {
+	public static void main(String[] args)
+	{	
+		// get the Array element as an input from user & printing all these elements....
+		String[] arr = new String[6];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the array elements for the first array:");
+		for(int i=0 ; i<6 ; i++)
+		{
+			String name = sc.next();
+			System.out.println(name);
+		}
+		
+			int[] arr1 = new int[5];
+			Scanner sc1 = new Scanner(System.in);
+			System.out.println("Enter the array elements for the second array:");
+			for(int j=0 ; j<5 ; j++)
+			{
+				int num = sc1.nextInt();
+				System.out.println(num);
+				
+			}
+			
+			
+					
+		
+		
+		
+		
+	}
+
+}
