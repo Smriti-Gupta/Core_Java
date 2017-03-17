e5271762fdb89d770602083b50c5348bf9430776
Smriti Gupta <smritigupta2202@gmail.com>
2017-03-17 11:18:31 +0530

    Add files via upload

---
+++ ExceptionOverride.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class ExcepOverride{
+	void ovrride() throws ArithmeticException
+	{
+		System.out.println("superclass method");
+	}
+}
+public class ExceptionOverride extends ExcepOverride {
+		void override() throws Exception
+		{
+			System.out.println("subclass method");
+		}
+			
+		public static void main(String[] args)
+		{
+			ExceptionOverride eo = new ExceptionOverride();
+			try{
+			
+			eo.override();
+			}
+			catch(Exception ex)
+			{
+				System.out.println("Exception Handeled");
+			}
+			}
+}
